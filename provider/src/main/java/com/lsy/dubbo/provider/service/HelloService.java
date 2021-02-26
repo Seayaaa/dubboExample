e6ad41cab166b84d1860d47a60b640c280acc5ea
lisiyu08 <lisiyu08@58.com>
2021-02-26 16:00:34 +0800

    first push

---
+++ provider/src/main/java/com/lsy/dubbo/provider/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.lsy.dubbo.provider.service;
+
+/**
+ * @author lsy
+ * @version 1.0
+ * @date 2021/2/19 18:00
+ */
+public interface HelloService {
+    public String sayHello(String name);
+}
---
+++ provider/src/main/java/com/lsy/dubbo/provider/service/HelloServiceImpl.java
@@ -0,0 +1,17 @@
+package com.lsy.dubbo.provider.service;
+
+import com.alibaba.dubbo.config.annotation.Service;
+
+/**
+ * @author lsy
+ * @version 1.0
+ * @date 2021/2/19 17:59
+ */
+@Service
+public class HelloServiceImpl implements HelloService {
+    
+    @Override
+    public String sayHello(String name) {
+        return "hello@@"+name;
+    }
+}
